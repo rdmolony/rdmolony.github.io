14492ab051c884382e0e1429e7e4106611880849
Rowan Molony <rowanmolony@gmail.com>
2025-04-26 23:23:34 +0100

    feat: remove storybookjs
    Not the right fit, meant for frontend components

--- flake.nix
+++ flake.nix
@@ -11,16 +11,12 @@
       run-tailwind = pkgs.writeShellScriptBin "run-tailwind" ''
         npx @tailwindcss/cli --watch -i ./tailwind.css -o ./assets/css/tailwind.css
       '';
-      run-storybook = pkgs.writeShellScriptBin "run-storybook" ''
-        npx storybook dev
-      '';
     in
     {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           run-jekyll
           run-tailwind
-          run-storybook
           ruby
           nodejs
         ];
